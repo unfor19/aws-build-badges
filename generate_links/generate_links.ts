b15032a6e2f7dc3ef911037d421246090223f578
Meir Gabay <unfor19@gmail.com>
2020-01-08 14:02:34 +0200

    ADD - generate_links testing

---
+++ generate_links/generate_links.ts
@@ -0,0 +1,61 @@
+// Used in aws_build_badges_cf_template.yml
+const AWS = require('aws-sdk');
+const response = require('cfn-response');
+
+const event = {
+    RequestType: 'Create',
+    ResponseURL: 'http://pre-signed-S3-url-for-response',
+    StackId: 'arn:aws:cloudformation:us-east-1:123456789012:stack/MyStack/guid',
+    RequestId: 'unique id for this create request',
+    ResourceType: 'Custom::TestResource',
+    LogicalResourceId: 'MyTestResource',
+    ResourceProperties: {
+        StackName: 'MyStack',
+        List: ['1', '2', '3'],
+    },
+};
+
+exports.handler = (event: any, context: any, callback: any) => {};
+// def generateUrl(bucketUrl, filename):
+//     s3 = boto3.client('s3')
+//     for o in objects:
+//         key = prefix + o
+//         copy_source = {
+//             'Bucket': source_bucket,
+//             'Key': key
+//         }
+//         print('copy_source: %s' % copy_source)
+//         print('dest_bucket = %s'%dest_bucket)
+//         print('key = %s' %key)
+//         s3.copy_object(CopySource=copy_source, Bucket=dest_bucket,
+//                 Key=key)
+// def delete_objects(bucket, prefix, objects):
+//     s3 = boto3.client('s3')
+//     objects = {'Objects': [{'Key': prefix + o} for o in objects]}
+//     s3.delete_objects(Bucket=bucket, Delete=objects)
+// def timeout(event, context):
+//     logging.error('Execution is about to time out, sending failure response to CloudFormation')
+//     cfnresponse.send(event, context, cfnresponse.FAILED, {}, None)
+// def handler(event, context):
+//     # make sure we send a failure to CloudFormation if the function
+//     # is going to timeout
+//     timer = threading.Timer((context.get_remaining_time_in_millis()
+//                 / 1000.00) - 0.5, timeout, args=[event, context])
+//     timer.start()
+//     print('Received event: %s' % json.dumps(event))
+//     status = cfnresponse.SUCCESS
+//     try:
+//         source_bucket = event['ResourceProperties']['SourceBucket']
+//         dest_bucket = event['ResourceProperties']['DestBucket']
+//         prefix = event['ResourceProperties']['Prefix']
+//         objects = event['ResourceProperties']['Objects']
+//         if event['RequestType'] == 'Delete':
+//             delete_objects(dest_bucket, prefix, objects)
+//         else:
+//             copy_objects(source_bucket, dest_bucket, prefix, objects)
+//     except Exception as e:
+//         logging.error('Exception: %s' % e, exc_info=True)
+//         status = cfnresponse.FAILED
+//     finally:
+//         timer.cancel()
+//         cfnresponse.send(event, context, status, {}, None)
